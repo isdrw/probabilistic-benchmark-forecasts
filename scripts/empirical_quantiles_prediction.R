62815981555c6308343d8b8fc8951dbd282ab5b4
Ismael Darwich <darwichismael@yahoo.de>
2025-10-06 12:03:46 +0200

    working function to calculate empirical quantiles for each row

--- scripts/empirical_quantiles_prediction.R
+++ scripts/empirical_quantiles_prediction.R
@@ -9,27 +9,78 @@ df <- read_parquet(file_path)
 
 
 #parameters
-#rolling window
+##rolling window
 R <- 11
+##quantiles
 tau <- 0.9
-#quantiles
 
 
-#calculating absolute forecast errors
+#data preparation
+##calculating absolute forecast errors
 df$abs_err <- abs(df$prediction-df$tv_1)
+##countries
+countries <- unique(df$country)
+
 
 #filter function for dataframe ("ngdp_rpch" for GDP or "pcpi_pch" for Inflation) 
-filter_df <- function(df, target_variable, h){
-  return(df[df$target == target_variable & df$horizon == h,])
+filter_df <- function(df, country, target_variable, h){
+  return(df[df$country == country & df$target == target_variable & df$horizon == h,])
 }
-filter_df(df, "pcpi_pch", 1)
+filter_df(df, countries[1], "pcpi_pch", 1)
 
-unique(df$country)
 
 #function to calculate empirical quantiles
-emp_q <- function(df, R, h, tau){
+emp_q <- function(df, country, target_variable, R, h, tau){
   #filtered df for given h
-  df_filtered <- df[df$horizon==h,]
+  df_filtered <- filter_df(df=df, country=country, target_variable=target_variable,h=h)
   
+  #empty output dataframe
+  df_quantiles <- data.frame(
+    country = character(),
+    target_variable = character(),
+    target_year = numeric(),
+    forecast_year = numeric(),
+    horizon = numeric(),
+    tau = numeric(),
+    abs_err = numeric(),
+    abs_err_quantile = numeric(),
+    stringsAsFactors = FALSE
+  )
+  
+  #loop over each target year
+  for(i in seq_along(df_filtered$target_year)){
+    #rolling window for each row (for first R-1 rows rolling window is smaller than R)   
+    rolling_window = min(c(i,R))
+    
+    #set of abs prediction errors 
+    abs_err_set <- df_filtered$abs_err[(i-rolling_window+1):i]
+    
+    #replacement of NA
+    indexes_na <- which(is.na(abs_err_set))
+    abs_err_set[indexes_na] <- abs_err_set[11-indexes_na+1]
+    
+    #quantiles of set
+    quantile <- quantile(abs_err_set, probs=tau, type=7, na.rm=TRUE)
+    
+    #append to output dataframe
+    new_row <- data.frame(
+      country = country,
+      target_variable = target_variable,
+      target_year = df_filtered$target_year[i],
+      forecast_year = df_filtered$forecast_year[i],
+      horizon = h,
+      tau = tau,
+      abs_err = df_filtered$abs_err[i],
+      abs_err_quantile = quantile,
+      stringsAsFactors = FALSE
+    )
+    df_quantiles <- rbind(df_quantiles, new_row)
+  }
+  
+  return(df_quantiles)
 }
+
+emp_q(df=df,country = countries[1], target_variable = "ngdp_rpch", R = R, h = 1,tau = tau)
+
+
 
