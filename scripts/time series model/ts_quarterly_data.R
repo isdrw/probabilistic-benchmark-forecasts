913ebe1e8d9b5c0aa02b61c25c816400a7db018b
Ismael Darwich <darwichismael@yahoo.de>
2025-10-24 17:45:42 +0200

    AR(1) predictions for quarterly data (up to 2024)

--- scripts/time series model/ts_quarterly_data.R
+++ scripts/time series model/ts_quarterly_data.R
@@ -5,6 +5,7 @@ gc()
 library(arrow)
 library(dplyr)
 library(tidyr)
+library(tseries)
 #path
 file_path <- r"(data/raw/IMF WEO\oecd_quarterly_data.csv)"
 
@@ -17,7 +18,7 @@ df <- df |>
 
 df$year <- as.numeric(df$year)
 df$quarter <- as.numeric(gsub("Q","",df$quarter))
-df_training <- df[df$year>=2012,]
+df_training <- df[df$year<=2012,]
 countries <- unique(df_training$ccode)
 
 filter_df <- function(df, filters) {
@@ -25,22 +26,76 @@ filter_df <- function(df, filters) {
     filter(across(all_of(names(filters)), ~ . == filters[[cur_column()]]))
 }
 
-
-fit_ar <- data.frame(
-  ccode = character(),
-  
-  stringsAsFactors = FALSE
+#Fit AR(1) on data for each country and target
+fit_ar_1 <- data.frame(
+  country=character(),
+  ar_1_gdp=numeric(),
+  intercept_gdp=numeric(),
+  aic_gdp=numeric(),
+  ar_1_cpi=numeric(),
+  intercept_cpi=numeric(),
+  aic_cpi=numeric()
+)
+predictions <- data.frame(
+  country=character(),
+  year=numeric(),
+  quarter=numeric(),
+  target=character(),
+  prediction=numeric()
 )
 for(country in countries){
+  #time series data for each country and target
+  data_gdp <- df_training[df_training$ccode==country,"gdp"]
+  data_cpi <- df_training[df_training$ccode==country,"cpi"]
+  #start date of observations (all start in the second quarter)
+  start_date <- c(min(df_training[df_training$ccode==country,"year"]),2)
   
+  #replace NAs with mean 
+  data_gdp[is.na(data_gdp)] <- mean(data_gdp,na.rm=TRUE)
+  data_cpi[is.na(data_cpi)] <- mean(data_cpi,na.rm=TRUE)
+  
+  #convert to time series object
+  ts_data_gdp <- ts(data_gdp,start = start_date,frequency = 4)
+  ts_data_cpi <- ts(data_cpi,start = start_date,frequency = 4)
+  
+  #simple AR(1) model fit
+  fit_gdp <- arima(ts_data_gdp,order = c(1,0,0))
+  fit_cpi <- arima(ts_data_cpi,order = c(1,0,0))
+  
+  new_row <- data.frame(
+    country=country,
+    ar_1_gdp=coef(fit_gdp)[1],
+    intercept_gdp=coef(fit_gdp)[2],
+    aic_gdp=fit_gdp$aic,
+    ar_1_cpi=coef(fit_cpi)[1],
+    intercept_cpi=coef(fit_cpi)[2],
+    aic_cpi=fit_cpi$aic
+  )
+  fit_ar_1 <- rbind(fit_ar_1,new_row)
+  #predict values for upcoming 12 years until 2024 starting with q1 2013
+  #4Q*12years = 48 predictions
+  prediction_gdp <- predict(fit_gdp,n.ahead = 48)
+  prediction_cpi <- predict(fit_cpi,n.ahead = 48)
+  
+  new_row_pred_gdp <- data.frame(
+    country=rep(country,times=48),
+    year=rep(2013:2024, each = 4),
+    quarter=rep(1:4,times=12),
+    target=rep("gdp",times=48),
+    prediction=prediction_gdp$pred
+  )
+  
+  new_row_pred_cpi <- data.frame(
+    country=rep(country,times=48),
+    year=rep(2013:2024, each = 4),
+    quarter=rep(1:4,times=12),
+    target=rep("cpi",times=48),
+    prediction=prediction_cpi$pred
+  )
+  
+  #append dataframe
+  predictions <- bind_rows(predictions, new_row_pred_gdp, new_row_pred_cpi)
+  rm(prediction_cpi,prediction_gdp,fit_gdp,fit_cpi,ts_data_cpi,ts_data_gdp)
 }
-data <- df$gdp
-data[is.na(data)] <- mean(data,na.rm=TRUE)
-ts_data <- ts(data)
 
-acf_values <- acf(ts_data,lag.max = 30,plot = TRUE)
-pacf_values <- pacf(ts_data,lag.max = 20, plot = TRUE)
 
-fit <- arima(ts_data,order=c(1,0,0))
-
-Box.test(residuals(fit),lag = 4,type = "Ljung-Box")
