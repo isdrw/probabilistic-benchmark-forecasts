05fb023f91ee7a1e8bdee91f12ef3183891fdcfb
Ismael Darwich <darwichismael@yahoo.de>
2026-01-03 21:26:08 +0100

    bqr

---
+++ scripts/bayesian quantile regression/bayesian quantile regression.R
@@ -0,0 +1,126 @@
+rm(list = ls())
+gc()
+
+#load library 
+library(arrow)
+library(dplyr)
+library(tidyr)
+library(nortest)
+library(fitdistrplus)
+library(forecast)
+library(quantreg)
+library(purrr)
+
+#source utility functions
+source("scripts/utilities/utility_functions.R")
+source("scripts/utilities/data_transformation_functions.R")
+
+#load and prepare WEO data
+df_weo <- load_and_prepare_WEO_data()
+
+#filter for G7 countries
+df_weo_g7 <- df_weo %>% filter(g7 == 1)
+
+#load and prepare data from file "data/processed/point_predictions_rw.csv" quarterly data
+df_rw <- load_and_prepare_RW_data() %>% aggregate_to_annual_input()
+
+#load and prepare data from file "data/processed/point_predictions_arima_1_0_0.csv" quarterly data
+df_ar1 <- load_and_prepare_ARIMA1_0_0_data() %>% aggregate_to_annual_input()
+
+#load and prepare data from file "data/processed/point_predictions_arima_1_1_0.csv" quarterly data
+df_arima1_1_0 <- load_and_prepare_ARIMA1_1_0_data() %>% aggregate_to_annual_input()
+
+fit_bqr <- function(df, country, tau, target, h, R=11){
+  
+  #prediction dataframe
+  predictions <- init_output_df()
+  
+  #output list 
+  out_list <- list()
+  index <- 1
+  
+  data_by_country <- df %>% 
+    filter(country == !!country, horizon == h) %>%
+    arrange(forecast_year, forecast_quarter)
+  
+  for(i in seq(R,nrow(data_by_country)-1)){
+    #predicted value vector
+    data_pred <- data_by_country[(i-R+1):i,][[paste0("pred_", target)]]
+    
+    #last prediction and truth value of point after rolling window
+    last_pred <- as.numeric(data_by_country[i+1,][[paste0("pred_", target)]])
+    truth_value <- as.numeric(data_by_country[[paste0("tv_", target)]][i+1])
+    
+    #truth value vector
+    data_tv1 <- data_by_country[(i-R+1):i,][[paste0("tv_", target)]]
+    
+    #start date of rolling window
+    forecast_year_start <- data_by_country[(i-R+1),"forecast_year"]
+    
+    #forecast year of point after rolling window for prediction
+    forecast_year_end <- data_by_country[i+1,"forecast_year"]
+    
+    #forecast quarter of point after rolling window for prediction
+    forecast_quarter_end <- as.numeric(data_by_country[i+1,"forecast_quarter"])
+    
+    #target year of point after rolling window for prediction
+    target_year_end <- data_by_country[i+1,"target_year"]
+    
+    #target quarter of point after rolling window for prediction
+    target_quarter_end <- (forecast_quarter_end + 4 * h - 1) %% 4 +1
+    
+    #skip if only NAs
+    if(all(is.na(data_pred)) || is.null(data_pred) || 
+       all(is.na(data_tv1)) || is.null(data_tv1)){
+      message("no valid data for ", country, " between ", 
+              forecast_year_start, " and ", forecast_year_end)
+      next
+    }
+    
+    #fit lqr model 
+    fit_l <- tryCatch({
+      bqr(data_tv1, data_pred, tau = (1-tau)/2)
+    },error=function(e){
+      message("Fit failed for ", country, " (", 
+              forecast_year_start, "–", forecast_year_end, "): ", e$message)
+      NULL
+    })
+    
+    fit_u <- tryCatch({
+      bqr(data_tv1, data_pred, tau = (1+tau)/2)
+    },error=function(e){
+      message("Fit failed for ", country, " (", 
+              forecast_year_start, "–", forecast_year_end, "): ", e$message)
+      NULL
+    })
+    
+    if(is.null(fit_l)||is.null(fit_u)){
+      next
+    }
+    
+    #prediction of quantile based on last point forecast
+    pred_l <- fit_l$beta[1] + fit_l$beta[2] * last_pred
+    pred_u <- fit_u$beta[1] + fit_u$beta[2] * last_pred
+    
+    #new row
+    out_list[[index]] <- new_pred_row(
+      country = country,
+      forecast_year = forecast_year_end,
+      target_year = target_year_end,
+      target_quarter = target_quarter_end,
+      target = target,
+      horizon = h,
+      tau = tau,
+      lower_bound = pred_l,
+      upper_bound = pred_u,
+      truth_value = truth_value,
+      prediction = last_pred
+    )
+    
+    index <- index + 1
+  }
+  
+  predictions <- bind_rows(out_list)
+  
+  return(predictions)
+}
